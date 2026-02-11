3e4d334b16691ddb5d793bfe71037d705793ad2a
Anu Govindhan <154439984+Anu-Govindhan-07@users.noreply.github.com>
2026-02-11 08:55:08 +0100

    Add final name tagging stage with persistent identity store

--- facevoice_fusion/src/name_tagging/__init__.py
+++ facevoice_fusion/src/name_tagging/__init__.py
@@ -1,3 +1,4 @@
 from .final_name_assignment import assign_names
+from .final_name_tagging import run_final_name_tagging
 
-__all__ = ["assign_names"]
+__all__ = ["assign_names", "run_final_name_tagging"]
